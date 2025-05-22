f194fc0c02eda12e72f93c746ec48d3205e39b1c
bagaretk <sebastianistrate26@gmail.com>
2025-05-22 15:00:02 +0300

    Create project

---
+++ src/main/java/acessa/PriceComparator/PriceComparatorApplication.java
@@ -0,0 +1,13 @@
+package acessa.PriceComparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceComparatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceComparatorApplication.class, args);
+	}
+
+}
